39a46bd6be3b04215236a477413b421f21de9f84
nadiaperesoficial-hash <nadiaperesoficial@gmail.com>
2026-06-28 18:42:03 -0300

    Update account_repo.dart

--- lib/account/account_repo.dart
+++ lib/account/account_repo.dart
@@ -5,7 +5,7 @@ import 'package:whixp/whixp.dart';
 
 abstract class AccountRepo {
   Stream<List<UiAccount>> get accounts;
-  Future<UiAccount?> register(XmppAccount account); // Mudado para Future para aguardar o login real
+  UiAccount register(XmppAccount account);
   void unregister(XmppAccount account);
 }
 
@@ -50,14 +50,11 @@ class AccountRepoImpl implements AccountRepo {
   Stream<List<UiAccount>> get accounts => _accountSubject.stream;
 
   @override
-  Future<UiAccount?> register(XmppAccount account) async {
+  UiAccount register(XmppAccount account) {
     final uiAccount = UiAccount(account);
     _accountsList.removeWhere((a) => a == uiAccount);
     _accountsList.add(uiAccount);
     _accountSubject.add(_accountsList);
-
-    // O Completer impede o código de avançar e quebrar a interface antes do login concluir
-    final loginCompleter = Completer<bool>();
 
     final client = Whixp(
       jabberID: '${account.username}@${account.domain}/simple_chat',
@@ -65,48 +62,34 @@ class AccountRepoImpl implements AccountRepo {
       host: account.domain,
       port: account.port,
       internalDatabasePath: 'whixp_${account.username}',
-      reconnectionPolicy: null, // Evita metralhadora de reconexões que reinicia a tela
-      useTLS: (account.port == 443 || account.port == 5223),
-      onBadCertificateCallback: (certificate) => true,
+      reconnectionPolicy: RandomBackoffReconnectionPolicy(1, 3),
+      logger: Log(enableWarning: true, enableError: true),
     );
 
     uiAccount._client = client;
     uiAccount.accountState = AccountRegistering(account: account);
 
-    client.addEventHandler<TransportState>('state', (state) {
-      if (state == null) return;
-
-      if (state == TransportState.connected) {
-        uiAccount.accountState = AccountRegistered(account: account);
-        if (!loginCompleter.isCompleted) loginCompleter.complete(true);
-      } else if (state == TransportState.disconnected) {
-        uiAccount.accountState = AccountUnregistered(
-          account: account,
-          message: 'Falha na autenticação do servidor.',
-        );
-        if (!loginCompleter.isCompleted) loginCompleter.complete(false);
-      }
+    // Evento correto conforme documentação oficial
+    client.addEventHandler<dynamic>('streamNegotiated', (_) {
+      client.sendPresence();
+      uiAccount.accountState = AccountRegistered(account: account);
     });
 
-    try {
-      client.connect();
-      
-      // Bloqueia a execução por até 10 segundos aguardando a resposta real do socket XMPP
-      final sucessoNoLogin = await loginCompleter.future.timeout(
-        const Duration(seconds: 10),
-        onTimeout: () => false,
+    client.addEventHandler<dynamic>('disconnected', (_) {
+      uiAccount.accountState = AccountUnregistered(
+        account: account,
+        message: 'Conexão encerrada',
       );
+    });
 
-      if (!sucessoNoLogin) {
-        // Se o login falhou, limpamos a instância e retornamos nulo para a UI saber o que houve
-        _accountsList.remove(uiAccount);
-        _accountSubject.add(_accountsList);
-        return null; 
-      }
-    } catch (e) {
-      return null;
-    }
+    client.addEventHandler<dynamic>('failed', (_) {
+      uiAccount.accountState = AccountUnregistered(
+        account: account,
+        message: 'Falha na autenticação',
+      );
+    });
 
+    client.connect();
     return uiAccount;
   }
 
